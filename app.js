b7dfbaa70421edf053d89fff6e91130a9f9c5221
rashik07 <saif67090707@gmail.com>
2024-10-23 19:09:19 +0600

    cors update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 app.use(express.static('public'));
 // const passportInfoRoute = require("./routes/passportInfo.route");
 const homeRoute = require("./routes/home.route");
@@ -14,13 +14,6 @@ const userRoute = require("./routes/user.route");
 // const wishlistRoute = require("./routes/wishlist.route");
 // const orderRoute = require("./routes/order.route");
 
-   // Cors
-   const corsOptions = {
-    origin: '*',
-    credentials: true,
-    optionSuccessStatus: 200
-  }
-  app.use(cors(corsOptions))
 
 app.get("/", (req, res) => {
   res.send("Route is working! YaY!");
