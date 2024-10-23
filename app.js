abe9d0cddef52affdb270b3011b8e0c63c460d5e
rashik07 <saif67090707@gmail.com>
2024-10-23 19:21:38 +0600

    cors update

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: 'https://www.keplerbd.org',
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.static('public'));
 // const passportInfoRoute = require("./routes/passportInfo.route");
 const homeRoute = require("./routes/home.route");
